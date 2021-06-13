0d9fbfad6cf703f091724f2ae7e134c19a5fb839
simonhaskelly <advance_user@hotmail.com>
2021-06-13 19:33:26 +0700

    IDK why this one works?

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-from fastapi import FastAPI, Depends, File, UploadFile
+from fastapi import FastAPI, Form, Depends, File, UploadFile
 from Model import *
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Cors handle
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.post('/user/')  # , response_model=User
-async def create_user_view(user: User, db: Session = Depends(get_db)):
-    db_user = create_user(db, user)
+
+# Routes
+# lallaalalal
+@app.post('/api/')  # , response_model=User
+async def add_row(db: Session = Depends(get_db), raw: RawBase = Depends(RawBase.as_form)):
+    db_user = add_raw(db, raw)
     return db_user
 
 
-@app.get('/user/', response_model=List[User])
-def get_user_view(db: Session = Depends(get_db)):
-    return get_users(db)
+# @app.get('/api/')  # , response_model=List[User]
+# def get_user_view(db: Session = Depends(get_db)):
+#     return get_users(db)
 
-
-@app.post('/file/')
-async def file_upload(file: UploadFile = File(...)):
-    print(file)
-
-# @app.get('/user/{user_uuid}')
-# def get_place_view(place_id: int, db: Session = Depends(get_db)):
-#     return get_place(db, place_id)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello?"}
